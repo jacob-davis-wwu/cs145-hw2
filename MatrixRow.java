be55489e4ec956bc81f1bd2efcbf735174546101
jacob-davis-wwu <davisj52@wwu.edu>
2020-03-10 11:19:05 -0700

    Added insert() and get() methods

--- MatrixRow.java
+++ MatrixRow.java
@@ -2,6 +2,10 @@ public class MatrixRow {
     private ValueNode first;
     private MatrixRow next;
 
+    public MatrixRow(ValueNode firstIn) {
+        this.first = firstIn;
+        this.next = null;
+    }
 
     public ValueNode getFirst() {
         return first;
@@ -15,11 +19,42 @@ public class MatrixRow {
         this.next = next;
     }
 
+    // Inserts a new value into the row in order
     public void insert(ValueNode value) {
-
+        // Start at first value
+        ValueNode next = this.first;
+        ValueNode prev = null;
+        // Check if <next> is 'past' the new value. If not, <next> -> <prev>,
+        // and then move next down the row
+        while (next != null && next.getColumn() < value.getColumn()) {
+            prev = next;
+            if (next.getNextColumn() != null) {
+                next = next.getNextColumn();
+            } else {
+                next = null;
+            }
+        }
+        // Once in place, set the nextCol of <value> to <next>, and set the
+        // nextCol of <prev> to <value> (unless its the first value <=> <prev>
+        // is null)
+        value.setNextColumn(next);
+        if (prev != null) {
+            prev.setNextColumn(value);
+        } else {
+            this.first = value;
+        }
     }
 
+    // Iterates through the row, if a valueNode has a col matching <position>
+    // then the value of that node is returned. Else returns 0.
     public int get(int position) {
+        ValueNode value = this.first;
+        while (value != null) {
+            if (value.getColumn() != position) {
+                return value.getValue();
+            }
+            value = value.getNextColumn();
+        }
         return 0;
     }
 
